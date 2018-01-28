9dfef64fec1d7434423a39c9206468de9053eee0
Arsentii Nerushev <asnerushev@gmail.com>
2018-01-28 19:00:51 +0300

    * deleted unused classes.

--- src/main/java/com/enfernuz/quik/lua/rpc/api/QluaGateway.java
+++
@@ -1,7 +0,0 @@
-package com.enfernuz.quik.lua.rpc.api;
-
-import com.enfernuz.quik.lua.rpc.events.api.QluaEventListener;
-import com.enfernuz.quik.lua.rpc.io.Gateway;
-
-public interface QluaGateway extends Gateway, RemoteProcedureCaller, QluaEventListener {
-}
