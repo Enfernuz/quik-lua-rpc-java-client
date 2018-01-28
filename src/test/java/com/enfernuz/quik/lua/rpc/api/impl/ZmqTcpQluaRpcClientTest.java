f80822c0f2ed452c56a311310ab2edb39b402593
Arsentii Nerushev <asnerushev@gmail.com>
2018-01-28 19:07:24 +0300

    * added an uncomplete unit test for future purposes.

---
+++ src/test/java/com/enfernuz/quik/lua/rpc/api/impl/ZmqTcpQluaRpcClientTest.java
@@ -0,0 +1,62 @@
+package com.enfernuz.quik.lua.rpc.api.impl;
+
+import com.google.protobuf.ByteString;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Captor;
+import org.mockito.Mockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.reflect.Whitebox;
+import qlua.rpc.IsConnected;
+import qlua.rpc.RPC;
+
+import java.io.IOException;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(ZmqTcpRpcGateway.class)
+public class ZmqTcpQluaRpcClientTest {
+
+    private ZmqTcpQluaRpcClient sut;
+
+    @Before
+    public void setup() throws IOException {
+
+        this.sut = ZmqTcpQluaRpcClient.create("127.0.0.1", 5560);
+
+
+    }
+
+    @After
+    public void teardown() {
+        this.sut = null;
+    }
+
+    @Test
+    public void test() throws Exception {
+
+        final ZmqTcpRpcGateway rpcGateway = mock(ZmqTcpRpcGateway.class);
+
+        doReturn(true).when(rpcGateway).isOpened();
+
+        Whitebox.setInternalState(sut, "rpcGateway", rpcGateway);
+
+        final RPC.ProcedureType procType = RPC.ProcedureType.IS_CONNECTED;
+        final IsConnected.Result expectedResult = IsConnected.Result.newBuilder().setIsConnected(1).build();
+        final RPC.Response expectedResponse = RPC.Response.newBuilder().setType(procType).setResult(expectedResult.toByteString()).build();
+
+        doReturn(expectedResponse).when(rpcGateway).call(RPC.ProcedureType.IS_CONNECTED);
+
+        final IsConnected.Result actualResult = sut.qlua_isConnected();
+
+        verify(rpcGateway).isOpened();
+        verify(rpcGateway).call(RPC.ProcedureType.IS_CONNECTED);
+
+        assertEquals(expectedResult.toByteString(), actualResult.toByteString());
+    }
+}
