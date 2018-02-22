015b71d1e5a3a07637515a42ed895b195e47aae3
Arsentii Nerushev <asnerushev@gmail.com>
2018-02-23 02:45:25 +0300

    * removed unused classes.

--- src/main/java/com/enfernuz/quik/lua/rpc/api/TcpQluaRpcClient.java
+++
@@ -1,6 +0,0 @@
-package com.enfernuz.quik.lua.rpc.api;
-
-import com.enfernuz.quik.lua.rpc.io.transport.TcpGateway;
-
-public interface TcpQluaRpcClient extends QluaRpcClient, TcpGateway {
-}
--- src/main/java/com/enfernuz/quik/lua/rpc/api/TcpRpcGateway.java
+++
@@ -1,7 +0,0 @@
-package com.enfernuz.quik.lua.rpc.api;
-
-import com.enfernuz.quik.lua.rpc.io.transport.TcpGateway;
-
-public interface TcpRpcGateway extends RpcGateway, TcpGateway {
-
-}
