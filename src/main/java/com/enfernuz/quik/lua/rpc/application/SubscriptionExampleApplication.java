274a543ddd2226418106dc6dde4934915b57542d
Arsentii Nerushev <asnerushev@gmail.com>
2018-02-23 04:28:15 +0300

    * added LoggingQluaEventHandler that simply logs incoming events.

--- src/main/java/com/enfernuz/quik/lua/rpc/application/SubscriptionExampleApplication.java
+++ src/main/java/com/enfernuz/quik/lua/rpc/application/SubscriptionExampleApplication.java
@@ -1,15 +1,10 @@
 package com.enfernuz.quik.lua.rpc.application;
 
 import com.enfernuz.quik.lua.rpc.api.security.zmq.*;
-import com.enfernuz.quik.lua.rpc.api.zmq.ZmqTcpQluaRpcClient;
-import com.enfernuz.quik.lua.rpc.api.zmq.impl.ZmqTcpQluaRpcClientImpl;
-import com.enfernuz.quik.lua.rpc.events.api.QluaEventHandler;
+import com.enfernuz.quik.lua.rpc.events.api.LoggingEventHandler;
 import com.enfernuz.quik.lua.rpc.events.impl.ZmqTcpQluaEventProcessor;
 import com.enfernuz.quik.lua.rpc.io.transport.NetworkAddress;
 import com.enfernuz.quik.lua.rpc.io.transport.SimpleNetworkAddress;
-import qlua.events.QluaEvents;
-import qlua.rpc.Message;
-import qlua.structs.QluaStructures;
 
 import java.util.concurrent.atomic.AtomicBoolean;
 
@@ -17,7 +12,7 @@ public class SubscriptionExampleApplication {
 
     public static void main(final String[] args) throws Exception {
 
-        // this is just for the testing purposes for now
+        // TO-DO: read from config file
 
         final NetworkAddress networkAddress = new SimpleNetworkAddress("127.0.0.1", 5561);
 
@@ -34,134 +29,7 @@ public class SubscriptionExampleApplication {
             final AtomicBoolean isServiceStopped = new AtomicBoolean(false);
 
             eventProcessor.subscribeToEverything();
-            eventProcessor.register(new QluaEventHandler() {
-                @Override
-                public void onClose() {
-                    isServiceStopped.set(true);
-                    System.out.println("ON CLOSE");
-                }
-
-                @Override
-                public void onStop() {
-                    isServiceStopped.set(true);
-                    System.out.println("ON STOP");
-                }
-
-                @Override
-                public void onConnected() {
-                    System.out.println("ON CONNECTED");
-                }
-
-                @Override
-                public void onDisconnected() {
-                    System.out.println("ON DISCONNECTED");
-                }
-
-                @Override
-                public void onInit() {
-                    System.out.println("ON INIT");
-                }
-
-                @Override
-                public void onFirm(QluaStructures.Firm firm) {
-                    System.out.println("ON FIRM: " + firm);
-                }
-
-                @Override
-                public void onAllTrade(QluaStructures.AllTrade allTrade) {
-                    System.out.println("ON ALL_TRADE: " + allTrade);
-                }
-
-                @Override
-                public void onTrade(QluaStructures.Trade trade) {
-                    System.out.println("ON TRADE: " + trade);
-                }
-
-                @Override
-                public void onOrder(QluaStructures.Order order) {
-                    System.out.println("ON ORDER: " + order);
-                }
-
-                @Override
-                public void onAccountBalance(QluaStructures.AccountBalance accountBalance) {
-                    System.out.println("ON ACCOUNT_BALANCE: " + accountBalance);
-                }
-
-                @Override
-                public void onFuturesLimitChange(QluaStructures.FuturesLimit futuresLimit) {
-                    System.out.println("ON FUTURES_LIMIT_CHANGE: " + futuresLimit);
-                }
-
-                @Override
-                public void onFuturesLimitDelete(QluaStructures.FuturesLimitDelete futuresLimitDelete) {
-                    System.out.println("ON FUTURES_LIMIT_DELETE: " + futuresLimitDelete);
-                }
-
-                @Override
-                public void onFuturesClientHolding(QluaStructures.FuturesClientHolding futuresClientHolding) {
-                    System.out.println("ON FUTURES_CLIENT_HOLDING: " + futuresClientHolding);
-                }
-
-                @Override
-                public void onMoneyLimit(QluaStructures.MoneyLimit moneyLimit) {
-                    System.out.println("ON MONEY_LIMIT: " + moneyLimit);
-                }
-
-                @Override
-                public void onMoneyLimitDelete(QluaStructures.MoneyLimitDelete moneyLimitDelete) {
-                    System.out.println("ON MONEY_LIMIT_DELETE: " + moneyLimitDelete);
-                }
-
-                @Override
-                public void onDepoLimit(QluaStructures.DepoLimit depoLimit) {
-                    System.out.println("ON DEPO_LIMIT: " + depoLimit);
-                }
-
-                @Override
-                public void onDepoLimitDelete(QluaStructures.DepoLimitDelete depoLimitDelete) {
-                    System.out.println("ON DEPO_LIMIT_DELETE: " + depoLimitDelete);
-                }
-
-                @Override
-                public void onAccountPosition(QluaStructures.AccountPosition accountPosition) {
-                    System.out.println("ON ACCOUNT_POSITION: " + accountPosition);
-                }
-
-                @Override
-                public void onNegDeal(QluaStructures.NegDeal negDeal) {
-                    System.out.println("ON NEG_DEAL: " + negDeal);
-                }
-
-                @Override
-                public void onNegTrade(QluaStructures.NegTrade negTrade) {
-                    System.out.println("ON NEG_TRADE: " + negTrade);
-                }
-
-                @Override
-                public void onStopOrder(QluaStructures.StopOrder stopOrder) {
-                    System.out.println("ON STOP_ORDER: " + stopOrder);
-                }
-
-                @Override
-                public void onTransReply(QluaStructures.Transaction transaction) {
-                    System.out.println("ON TRANS_REPLY: " + transaction);
-                }
-
-                @Override
-                public void onParam(QluaStructures.ParamEventInfo param) {
-                    System.out.println("ON PARAM: " + param);
-                }
-
-                @Override
-                public void onQuote(QluaStructures.QuoteEventInfo quote) {
-                    System.out.println("ON QUOTE: " + quote);
-                }
-
-                @Override
-                public void onCleanUp() {
-                    System.out.println("ON CLEAN_UP");
-                }
-            });
+            eventProcessor.register(LoggingEventHandler.INSTANCE);
 
             eventProcessor.open();
 
---
+++ src/main/java/com/enfernuz/quik/lua/rpc/events/api/LoggingEventHandler.java
@@ -0,0 +1,136 @@
+package com.enfernuz.quik.lua.rpc.events.api;
+
+import org.slf4j.*;
+import qlua.structs.QluaStructures;
+
+public enum LoggingEventHandler implements QluaEventHandler {
+
+    INSTANCE;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(LoggingEventHandler.class);
+
+    @Override
+    public void onClose() {
+        LOGGER.info("onClose");
+    }
+
+    @Override
+    public void onStop() {
+        LOGGER.info("onStop");
+    }
+
+    @Override
+    public void onConnected() {
+        LOGGER.info("onConnected");
+    }
+
+    @Override
+    public void onDisconnected() {
+        LOGGER.info("onDisconnected");
+    }
+
+    @Override
+    public void onInit() {
+        LOGGER.info("onInit");
+    }
+
+    @Override
+    public void onFirm(QluaStructures.Firm firm) {
+        LOGGER.info("onFirm: {}", firm);
+    }
+
+    @Override
+    public void onAllTrade(QluaStructures.AllTrade allTrade) {
+        LOGGER.info("onAllTrade: {}", allTrade);
+    }
+
+    @Override
+    public void onTrade(QluaStructures.Trade trade) {
+        LOGGER.info("onTrade: {}", trade);
+    }
+
+    @Override
+    public void onOrder(QluaStructures.Order order) {
+        LOGGER.info("onOrder: {}", order);
+    }
+
+    @Override
+    public void onAccountBalance(QluaStructures.AccountBalance accountBalance) {
+        LOGGER.info("onAccountBalance: {}", accountBalance);
+    }
+
+    @Override
+    public void onFuturesLimitChange(QluaStructures.FuturesLimit futuresLimit) {
+        LOGGER.info("onFuturesLimitChange: {}", futuresLimit);
+    }
+
+    @Override
+    public void onFuturesLimitDelete(QluaStructures.FuturesLimitDelete futuresLimitDelete) {
+        LOGGER.info("onFuturesLimitDelete: {}", futuresLimitDelete);
+    }
+
+    @Override
+    public void onFuturesClientHolding(QluaStructures.FuturesClientHolding futuresClientHolding) {
+        LOGGER.info("onFuturesClientHolding: {}", futuresClientHolding);
+    }
+
+    @Override
+    public void onMoneyLimit(QluaStructures.MoneyLimit moneyLimit) {
+        LOGGER.info("onMoneyLimit: {}", moneyLimit);
+    }
+
+    @Override
+    public void onMoneyLimitDelete(QluaStructures.MoneyLimitDelete moneyLimitDelete) {
+        LOGGER.info("onMoneyLimitDelete: {}", moneyLimitDelete);
+    }
+
+    @Override
+    public void onDepoLimit(QluaStructures.DepoLimit depoLimit) {
+        LOGGER.info("onDepoLimit: {}", depoLimit);
+    }
+
+    @Override
+    public void onDepoLimitDelete(QluaStructures.DepoLimitDelete depoLimitDelete) {
+        LOGGER.info("onDepoLimitDelete: {}", depoLimitDelete);
+    }
+
+    @Override
+    public void onAccountPosition(QluaStructures.AccountPosition accountPosition) {
+        LOGGER.info("onAccountPosition: {}", accountPosition);
+    }
+
+    @Override
+    public void onNegDeal(QluaStructures.NegDeal negDeal) {
+        LOGGER.info("onNegDeal: {}", negDeal);
+    }
+
+    @Override
+    public void onNegTrade(QluaStructures.NegTrade negTrade) {
+        LOGGER.info("onNegTrade: {}", negTrade);
+    }
+
+    @Override
+    public void onStopOrder(QluaStructures.StopOrder stopOrder) {
+        LOGGER.info("onStopOrder: {}", stopOrder);
+    }
+
+    @Override
+    public void onTransReply(QluaStructures.Transaction transaction) {
+        LOGGER.info("onTransReply: {}", transaction);
+    }
+
+    @Override
+    public void onParam(QluaStructures.ParamEventInfo param) {
+        LOGGER.info("onParam: {}", param);
+    }
+
+    @Override
+    public void onQuote(QluaStructures.QuoteEventInfo quote) {
+        LOGGER.info("onQuote: {}", quote);
+    }
+
+    @Override
+    public void onCleanUp() {
+        LOGGER.info("onCleanUp");
+    }
+}
