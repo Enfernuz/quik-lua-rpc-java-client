df16103b4dd7b198403d717ffa43e5bf47ea3f90
Arsentii Nerushev <asnerushev@gmail.com>
2018-03-17 15:11:28 +0300

    * WIP: #3.

--- src/main/java/com/enfernuz/quik/lua/rpc/api/security/zmq/CurveCredentials.java
+++ src/main/java/com/enfernuz/quik/lua/rpc/api/security/zmq/CurveCredentials.java
@@ -1,8 +1,19 @@
 package com.enfernuz.quik.lua.rpc.api.security.zmq;
 
+/**
+ * Учётные данные клиента для защитного механизма ZeroMQ CURVE.
+ */
 public interface CurveCredentials {
 
+    /**
+     * Получить публичный CURVE-ключ сервера для данного экземпляра учётных данных.
+     * @return публичный CURVE-ключ сервера для данного экземпляра учётных данных
+     */
     CurveKey getServerPublicKey();
 
+    /**
+     * Получить ключевую пару CURVE клиента для данного экземпляра учётных данных.
+     * @return ключевая пара CURVE клиента для данного экземпляра учётных данных
+     */
     CurveKeyPair getClientKeyPair();
 }
--- src/main/java/com/enfernuz/quik/lua/rpc/api/security/zmq/CurveKeyPair.java
+++ src/main/java/com/enfernuz/quik/lua/rpc/api/security/zmq/CurveKeyPair.java
@@ -6,21 +6,40 @@ import java.util.Objects;
 
 import static java.util.Objects.requireNonNull;
 
+/**
+ * Ключевая пара CURVE для защитного механизма ZeroMQ CURVE.
+ * Консистентность ключевой пары (соответствие публичной и секретной частей) не является целью данного класса, поэтому
+ * наличие экземпляров с ключами из разных пар вполне возможно (хоть и не имеет особого смысла).
+ */
 public final class CurveKeyPair {
 
     private final CurveKey publicKey;
     private final CurveKey secretKey;
 
+    /**
+     * Создать экземпляр ключевой пары CURVE.
+     * @param publicKey публичный CURVE-ключ
+     * @param secretKey секретный CURVE-ключ
+     * @throws NullPointerException если какой либо из аргументов является {@code null}
+     */
     public CurveKeyPair(final CurveKey publicKey, final CurveKey secretKey) {
 
         this.publicKey = requireNonNull(publicKey, "The argument \"publicKey\" must not be null.");
         this.secretKey = requireNonNull(secretKey, "The argument \"secretKey\" must not be null.");
     }
 
+    /**
+     * Получить публичный CURVE-ключ данной ключевой пары.
+     * @return публичный CURVE-ключ данной ключевой пары.
+     */
     public CurveKey getPublicKey() {
         return publicKey;
     }
 
+    /**
+     * Получить секретный CURVE-ключ данной ключевой пары.
+     * @return секретный CURVE-ключ данной ключевой пары.
+     */
     public CurveKey getSecretKey() {
         return secretKey;
     }
