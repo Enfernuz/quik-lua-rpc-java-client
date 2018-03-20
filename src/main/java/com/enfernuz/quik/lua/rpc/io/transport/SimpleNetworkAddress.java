e55c5169d1e9f6edfbb7fd725dc9e184b720fde5
Arsentii Nerushev <asnerushev@gmail.com>
2018-03-21 02:10:15 +0300

    * WIP: #3.

--- src/main/java/com/enfernuz/quik/lua/rpc/io/transport/SimpleNetworkAddress.java
+++ src/main/java/com/enfernuz/quik/lua/rpc/io/transport/SimpleNetworkAddress.java
@@ -1,19 +1,41 @@
 package com.enfernuz.quik.lua.rpc.io.transport;
 
+import com.google.common.base.Strings;
+
 import java.util.Objects;
 
-import static java.util.Objects.requireNonNull;
+import static com.google.common.base.Preconditions.checkArgument;
 
+/**
+ * Базовая реализация сетевого адреса.
+ */
 public final class SimpleNetworkAddress implements NetworkAddress {
 
     private final String host;
     private final int port;
 
+    /**
+     * Создаёт экземпляр сетевого адреса с заданным наименованием хоста и портом.
+     *
+     * @param host  строковое наименование хоста, представляющее собой доменное имя или IP-адрес
+     * @param port  порт
+     * @throws IllegalArgumentException если указанное строковое наименование является null или пустой строкой
+     * @throws IllegalArgumentException если указанный порт не лежит в диапазоне от 0 до 65535 включительно
+     */
     public SimpleNetworkAddress(final String host, final int port) {
 
-        // TODO: add validation
-        this.host = requireNonNull(host);
-        this.port  = port;
+        checkArgument(
+                port >= 0 && port <= 65535,
+                "Порт должен быть целым числом в диапазоне от 0 до 65535 включительно."
+        );
+
+        checkArgument(
+                !Strings.isNullOrEmpty(host),
+                "Наименование хоста должно быть не-null непустой строкой."
+        );
+
+        this.host = host;
+        this.port = port;
     }
 
     @Override
@@ -44,6 +66,11 @@ public final class SimpleNetworkAddress implements NetworkAddress {
         return Objects.hash(host, port);
     }
 
+    /**
+     * Возвращает строковое представление сетевого адреса.
+     *
+     * @return строка формата {@link SimpleNetworkAddress#getHost()}:{@link SimpleNetworkAddress#getPort()}
+     */
     @Override
     public String toString() {
         return String.format("%s:%d", host, port);
