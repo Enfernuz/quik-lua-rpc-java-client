f5d9d09012772ca911045e0c09119e9ca90dc380
Arsentii Nerushev <asnerushev@gmail.com>
2018-03-21 22:45:26 +0300

    * WIP: #3.

--- src/main/java/com/enfernuz/quik/lua/rpc/api/RpcGateway.java
+++ src/main/java/com/enfernuz/quik/lua/rpc/api/RpcGateway.java
@@ -3,5 +3,13 @@ package com.enfernuz.quik.lua.rpc.api;
 import com.enfernuz.quik.lua.rpc.api.security.Securable;
 import com.enfernuz.quik.lua.rpc.io.Gateway;
 
+/**
+ * Компонент <b>Шлюз удалённого вызова процедур</b>, удовлетворяющий следующим критериям:
+ * <ul>
+ *     <li>1. является {@link Gateway шлюзом}</li>
+ *     <li>2. способен выполнять удалённый вызов процедур к API QLua терминала QUIK</li>
+ *     <li>3. поддерживает различные механизмы защиты передачи данных</li>
+ * </ul>
+ */
 public interface RpcGateway extends RemoteProcedureCaller, Gateway, Securable {
 }
--- src/main/java/com/enfernuz/quik/lua/rpc/api/zmq/ZmqTcpRpcGateway.java
+++ src/main/java/com/enfernuz/quik/lua/rpc/api/zmq/ZmqTcpRpcGateway.java
@@ -2,5 +2,12 @@ package com.enfernuz.quik.lua.rpc.api.zmq;
 
 import com.enfernuz.quik.lua.rpc.io.transport.TcpGateway;
 
+/**
+ * Компонент <b>TCP-шлюз удалённого вызова процедур на базе ZeroMQ</b>, удовлетворяющий следующим критериям:
+ * <ul>
+ *     <li>1. является {@link ZmqRpcGateway шлюзом удалённого вызова процедур на базе ZeroMQ}</li>
+ *     <li>2. является {@link TcpGateway TCP-шлюзом}</li>
+ * </ul>
+ */
 public interface ZmqTcpRpcGateway extends ZmqRpcGateway, TcpGateway {
 }
