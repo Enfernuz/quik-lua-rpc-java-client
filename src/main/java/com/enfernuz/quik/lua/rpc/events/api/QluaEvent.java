49ea524c44fb5ee3f9108d3ec8b05a655c547ad0
Arsentii Nerushev <asnerushev@gmail.com>
2018-03-27 23:53:32 +0300

    * WIP: #3.

--- src/main/java/com/enfernuz/quik/lua/rpc/events/api/QluaEvent.java
+++ src/main/java/com/enfernuz/quik/lua/rpc/events/api/QluaEvent.java
@@ -8,17 +8,40 @@ import java.util.Objects;
 
 import static java.util.Objects.requireNonNull;
 
-public final class QluaEvent {
+/**
+ * Событие терминала QUIK в рамках API QLua.
+ * <p/>
+ * В QLua оповещения о событиях терминала QUIK приходят посредством функций обратного вызова (callbacks). Тип события и
+ * его данные сериализуются RPC-сервисом на стороне терминала QUIK с помощью Google Protocol Buffers и передаются
+ * клиентам.
+ */
+public class QluaEvent {
 
     private final QluaEvents.EventType type;
     private final ByteString data;
 
+    /**
+     * Создаёт экземпляр события API QLua терминала QUIK с заданным типом и данными,
+     * сериализованными в байтовую строку с помощью Google Protocol Buffers.
+     *
+     * @param type  тип события
+     * @param data  данные события, сериализованные в байтовую строку с помощью Google Protocol Buffers
+     * @return экземпляр события API QLua терминала QUIK с заданным типом и данными, сериализованными в байтовую строку
+     * с помощью Google Protocol Buffers
+     * @throws NullPointerException если заданный тип события является null
+     */
     public static QluaEvent create(final QluaEvents.EventType type, final ByteString data) {
 
         requireNonNull(type, "The argument 'type' must not be null.");
         return new QluaEvent(type, data);
     }
 
+    /**
+     * Создаёт экземпляр события API QLua терминала QUIK с заданным типом.
+     *
+     * @param type  тип события
+     * @return экземпляр события API QLua терминала QUIK
+     */
     public static QluaEvent create(final QluaEvents.EventType type) {
         return create(type, null);
     }
@@ -29,10 +52,20 @@ public final class QluaEvent {
         this.data = data;
     }
 
+    /**
+     * Получает тип события.
+     *
+     * @return тип события
+     */
     public QluaEvents.EventType getType() {
         return type;
     }
 
+    /**
+     * Получает данные события, сериализованные в байтовую строку с помощью Google Protocol Buffers.
+     *
+     * @return данные события, сериализованные в байтовую строку с помощью Google Protocol Buffers
+     */
     public ByteString getData() {
         return data;
     }
