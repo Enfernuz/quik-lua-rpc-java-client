bd5ac0d71474fb1d0998b27373c34065dccfd0e7
Arsentii Nerushev <asnerushev@gmail.com>
2018-06-05 23:53:00 +0500

    Fixed Travis build. Finally.

--- install-protobuf.sh
+++ install-protobuf.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -ex
-wget https://github.com/google/protobuf/releases/download/v3.5.1/protoc-3.5.1-linux-x86_32.zip
-unzip protoc-3.5.1-linux-x86_32.zip -d $HOME/protobuf
+wget https://github.com/google/protobuf/releases/download/v3.5.1/protoc-3.5.1-linux-x86_64.zip
+unzip protoc-3.5.1-linux-x86_64.zip -d $HOME/protobuf
