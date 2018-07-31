5ee2419343d43f3f74ebbd295b2c90b47a348e7e
Arsentii Nerushev <asnerushev@gmail.com>
2018-08-01 01:48:16 +0300

    Added request-result pair for GetFuturesHolding.

---
+++ src/main/java/com/enfernuz/quik/lua/rpc/api/structures/GetFuturesHolding.java
@@ -0,0 +1,57 @@
+package com.enfernuz.quik.lua.rpc.api.structures;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.base.MoreObjects;
+import com.sun.istack.internal.NotNull;
+import lombok.Builder;
+import lombok.NonNull;
+import lombok.Value;
+
+public final class GetFuturesHolding {
+
+    private GetFuturesHolding() {}
+
+    @Value
+    public static class Request {
+
+        @JsonProperty("firmid") String firmId;
+        @JsonProperty("trdaccid") String trdAccId;
+        @JsonProperty("sec_code") String secCode;
+        @JsonProperty("type") int type;
+
+        @Builder
+        private Request(
+                final @NonNull String firmId,
+                final @NonNull String trdAccId,
+                final @NonNull String secCode,
+                final int type) {
+
+            this.firmId = firmId;
+            this.trdAccId = trdAccId;
+            this.secCode = secCode;
+            this.type = type;
+        }
+
+        @Override
+        public String toString() {
+            return MoreObjects.toStringHelper(this)
+                    .add("firmid", firmId)
+                    .add("trdaccid", trdAccId)
+                    .add("sec_code", secCode)
+                    .add("type", type)
+                    .toString();
+        }
+    }
+
+    @Value
+    public static class Result {
+
+        FuturesClientHolding futuresHolding;
+
+        @JsonCreator
+        public Result(final @JsonProperty(value = "futures_holding", required = true) @NotNull FuturesClientHolding futuresHolding) {
+            this.futuresHolding = futuresHolding;
+        }
+    }
+}
