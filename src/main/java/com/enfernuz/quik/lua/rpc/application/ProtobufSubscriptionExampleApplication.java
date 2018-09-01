5ccbdc54a023ffd389e95150608cc309a952af41
Arsentii Nerushev <asnerushev@gmail.com>
2018-09-01 18:00:51 +0300

    Applying the Lombok's magic for logging.

--- src/main/java/com/enfernuz/quik/lua/rpc/application/ProtobufSubscriptionExampleApplication.java
+++ src/main/java/com/enfernuz/quik/lua/rpc/application/ProtobufSubscriptionExampleApplication.java
@@ -7,24 +7,21 @@ import com.enfernuz.quik.lua.rpc.events.api.PollingMode;
 import com.enfernuz.quik.lua.rpc.events.api.QluaEvent;
 import com.enfernuz.quik.lua.rpc.events.api.QluaEventProcessor;
 import com.enfernuz.quik.lua.rpc.api.zmq.ZmqTcpQluaEventProcessor;
-import com.enfernuz.quik.lua.rpc.serde.protobuf.ProtobufSerdeModule;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.File;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicBoolean;
 
+@Slf4j
 public class ProtobufSubscriptionExampleApplication {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(SubscriptionExampleApplication.class);
 
     public static void main(final String[] args) {
 
         final String filePath = args[0];
         if (filePath == null) {
-            LOGGER.error("Не задан путь до файла конфигурации.");
+            log.error("Не задан путь до файла конфигурации.");
             return;
         }
 
@@ -32,32 +29,32 @@ public class ProtobufSubscriptionExampleApplication {
         try {
             configFile = new File(filePath);
         } catch (final Exception ex) {
-            LOGGER.error(String.format("Не удалось прочитать файл '%s'.", filePath), ex);
+            log.error(String.format("Не удалось прочитать файл '%s'.", filePath), ex);
             return;
         }
 
-        LOGGER.info("Чтение файла конфигурации...");
+        log.info("Чтение файла конфигурации...");
         ClientConfiguration config;
         try {
             config = JsonClientConfigurationReader.INSTANCE.read(configFile);
         } catch (final Exception ex) {
-            LOGGER.error(String.format("Не удалось получить объект конфигурации из файла '%s'.", filePath), ex);
+            log.error(String.format("Не удалось получить объект конфигурации из файла '%s'.", filePath), ex);
             return;
         }
 
-        LOGGER.info("Инициализация клиента...");
+        log.info("Инициализация клиента...");
         final ExecutorService stdinScannerExecutorService = Executors.newSingleThreadExecutor();
         try (final ZmqTcpQluaEventProcessor eventProcessor = ZmqTcpQluaEventProcessor.newInstance(config, PollingMode.BLOCKING)) {
 
-            LOGGER.info("Подписка на все события...");
+            log.info("Подписка на все события...");
             //eventProcessor.subscribeToEverything();
             eventProcessor.subscribe(QluaEvent.EventType.ON_FIRM);
             eventProcessor.subscribe(QluaEvent.EventType.ON_MONEY_LIMIT);
 
-            LOGGER.info("Регистрация обработчиков событий...");
+            log.info("Регистрация обработчиков событий...");
             eventProcessor.register(LoggingEventHandler.INSTANCE);
 
-            LOGGER.info("Соединение с RPC-сервисом...");
+            log.info("Соединение с RPC-сервисом...");
             eventProcessor.open();
 
             // monitor the Enter key pressing
@@ -67,20 +64,20 @@ public class ProtobufSubscriptionExampleApplication {
                 try {
                     enough.set(System.in.read() > 0);
                 } catch (final Exception ex) {
-                    LOGGER.error("Ошибка при чтении стандартного потока ввода.", ex);
+                    log.error("Ошибка при чтении стандартного потока ввода.", ex);
                 }
             });
 
-            LOGGER.info("Начало обработки событий. Нажмите Enter для остановки...");
+            log.info("Начало обработки событий. Нажмите Enter для остановки...");
             while ( !enough.get() ) {
                 eventProcessor.process(1);
             }
 
-            LOGGER.info("Выход из программы...");
+            log.info("Выход из программы...");
         } catch (final QluaEventProcessor.QluaEventProcessingException ex) {
-            LOGGER.error("Ошибка при обработке события.", ex);
+            log.error("Ошибка при обработке события.", ex);
         } catch (final Exception ex) {
-            LOGGER.error("Не удалось начать обработку событий.", ex);
+            log.error("Не удалось начать обработку событий.", ex);
         } finally {
             stdinScannerExecutorService.shutdownNow();
         }
--- src/main/java/com/enfernuz/quik/lua/rpc/application/RpcExampleApplication.java
+++ src/main/java/com/enfernuz/quik/lua/rpc/application/RpcExampleApplication.java
@@ -4,20 +4,18 @@ import com.enfernuz.quik.lua.rpc.api.messages.Message;
 import com.enfernuz.quik.lua.rpc.api.zmq.ZmqTcpQluaRpcClient;
 import com.enfernuz.quik.lua.rpc.config.ClientConfiguration;
 import com.enfernuz.quik.lua.rpc.config.JsonClientConfigurationReader;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.File;
 
+@Slf4j
 public class RpcExampleApplication {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(RpcExampleApplication.class);
 
     public static void main(final String[] args) {
 
         final String filePath = args[0];
         if (filePath == null) {
-            LOGGER.error("Не задан путь до файла конфигурации.");
+            log.error("Не задан путь до файла конфигурации.");
             return;
         }
 
@@ -25,34 +23,34 @@ public class RpcExampleApplication {
         try {
             configFile = new File(filePath);
         } catch (final Exception ex) {
-            LOGGER.error(String.format("Не удалось прочитать файл '%s'.", filePath), ex);
+            log.error(String.format("Не удалось прочитать файл '%s'.", filePath), ex);
             return;
         }
 
-        LOGGER.info("Чтение файла конфигурации...");
+        log.info("Чтение файла конфигурации...");
         ClientConfiguration config;
         try {
             config = JsonClientConfigurationReader.INSTANCE.read(configFile);
         } catch (final Exception ex) {
-            LOGGER.error(String.format("Не удалось получить объект конфигурации из файла '%s'.", filePath), ex);
+            log.error(String.format("Не удалось получить объект конфигурации из файла '%s'.", filePath), ex);
             return;
         }
 
-        LOGGER.info("Инициализация клиента...");
+        log.info("Инициализация клиента...");
         try (final ZmqTcpQluaRpcClient rpcClient = ZmqTcpQluaRpcClient.newInstance(config)) {
 
-            LOGGER.info("Соединение с RPC-сервисом...");
+            log.info("Соединение с RPC-сервисом...");
             rpcClient.open();
 
             final Message.Request request = new Message.Request("Hello, world!", Message.IconType.WARNING);
 
-            LOGGER.info("Выполнение удалённой процедуры 'message' на терминале QUIK...");
+            log.info("Выполнение удалённой процедуры 'message' на терминале QUIK...");
             final Message.Result result = rpcClient.qlua_message(request);
 
-            LOGGER.info("Результат выполнения удалённой процедуры 'message': {}.", result.getResult());
-            LOGGER.info("Выход из программы...");
+            log.info("Результат выполнения удалённой процедуры 'message': {}.", result.getResult());
+            log.info("Выход из программы...");
         } catch (final Exception ex) {
-            LOGGER.error("Не удалось выполнить удалённый вызов процедуры.", ex);
+            log.error("Не удалось выполнить удалённый вызов процедуры.", ex);
         }
     }
 }
