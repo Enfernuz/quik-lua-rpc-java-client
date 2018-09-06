7fa5b4a134ee6cb913915962a08999f3a4d26b87
Arsentii Nerushev <asnerushev@gmail.com>
2018-09-07 02:10:27 +0300

    Removed a SUT's self-dependency, which improves the test's correctness.

--- src/test/java/com/enfernuz/quik/lua/rpc/serde/protobuf/DepoLimitDeletePbSerdeTest.java
+++ src/test/java/com/enfernuz/quik/lua/rpc/serde/protobuf/DepoLimitDeletePbSerdeTest.java
@@ -1,7 +1,6 @@
 package com.enfernuz.quik.lua.rpc.serde.protobuf;
 
 import com.enfernuz.quik.lua.rpc.api.structures.DepoLimitDelete;
-import com.enfernuz.quik.lua.rpc.serde.PbConverter;
 import com.enfernuz.quik.lua.rpc.serde.SerdeModule;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -15,7 +14,6 @@ import static org.junit.Assert.assertTrue;
 public class DepoLimitDeletePbSerdeTest {
 
     private static SerdeModule sut;
-    private static PbConverter<QluaStructures.DepoLimitDelete, DepoLimitDelete> pbConverter;
 
     private static DepoLimitDelete expectedObject;
     private static byte[] expectedPbInput;
@@ -27,7 +25,6 @@ public class DepoLimitDeletePbSerdeTest {
     public static void globalSetup() {
 
         sut = ProtobufSerdeModule.INSTANCE;
-        pbConverter = DepoLimitDeletePbSerde.INSTANCE;
 
         expectedObject = DepoLimitDelete.builder()
                 .secCode("1")
@@ -36,13 +33,22 @@ public class DepoLimitDeletePbSerdeTest {
                 .clientCode("4")
                 .limitKind(5)
                 .build();
-        expectedPbInput = pbConverter.convertToPb(expectedObject).toByteArray();
+        expectedPbInput = QluaStructures.DepoLimitDelete.newBuilder()
+                .setSecCode("1")
+                .setTrdaccid("2")
+                .setFirmid("3")
+                .setClientCode("4")
+                .setLimitKind(5)
+                .build()
+                .toByteArray();
 
         expectedObjectWithNullNonRequiredStringFileds = DepoLimitDelete.builder()
                 .limitKind(1)
                 .build();
-        expectedPbInputWithEmptyNonRequiredStringFields =
-                pbConverter.convertToPb(expectedObjectWithNullNonRequiredStringFileds).toByteArray();
+        expectedPbInputWithEmptyNonRequiredStringFields = QluaStructures.DepoLimitDelete.newBuilder()
+                .setLimitKind(1)
+                .build()
+                .toByteArray();
     }
 
     @Test
