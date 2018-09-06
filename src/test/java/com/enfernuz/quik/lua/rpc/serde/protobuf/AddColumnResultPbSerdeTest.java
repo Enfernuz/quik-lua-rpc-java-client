dea8c735fa70645bd2db37ecf21f0402eafb2bdb
Arsentii Nerushev <asnerushev@gmail.com>
2018-09-07 02:02:33 +0300

    Removed a SUT's self-dependency, which improves the test's correctness.

--- src/test/java/com/enfernuz/quik/lua/rpc/serde/protobuf/AddColumnResultPbSerdeTest.java
+++ src/test/java/com/enfernuz/quik/lua/rpc/serde/protobuf/AddColumnResultPbSerdeTest.java
@@ -1,7 +1,6 @@
 package com.enfernuz.quik.lua.rpc.serde.protobuf;
 
 import com.enfernuz.quik.lua.rpc.api.messages.AddColumn;
-import com.enfernuz.quik.lua.rpc.serde.PbConverter;
 import com.enfernuz.quik.lua.rpc.serde.SerdeModule;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -14,7 +13,6 @@ import static org.junit.Assert.assertTrue;
 public class AddColumnResultPbSerdeTest {
 
     private static SerdeModule sut;
-    private static PbConverter<qlua.rpc.AddColumn.Result, AddColumn.Result> pbConverter;
 
     private static AddColumn.Result expectedObject;
     private static byte[] expectedPbInput;
@@ -23,10 +21,12 @@ public class AddColumnResultPbSerdeTest {
     public static void globalSetup() {
 
         sut = ProtobufSerdeModule.INSTANCE;
-        pbConverter = AddColumnResultPbSerde.INSTANCE;
 
         expectedObject = new AddColumn.Result(1);
-        expectedPbInput = pbConverter.convertToPb(expectedObject).toByteArray();
+        expectedPbInput = qlua.rpc.AddColumn.Result.newBuilder()
+                .setResult(1)
+                .build()
+                .toByteArray();
     }
 
     @Test
