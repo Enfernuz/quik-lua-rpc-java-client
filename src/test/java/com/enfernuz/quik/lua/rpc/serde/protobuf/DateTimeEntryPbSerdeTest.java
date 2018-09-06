fcc0c0a5c10fd30f402fca5b83908454a849584b
Arsentii Nerushev <asnerushev@gmail.com>
2018-09-07 02:08:57 +0300

    Removed a SUT's self-dependency, which improves the test's correctness.

--- src/test/java/com/enfernuz/quik/lua/rpc/serde/protobuf/DateTimeEntryPbSerdeTest.java
+++ src/test/java/com/enfernuz/quik/lua/rpc/serde/protobuf/DateTimeEntryPbSerdeTest.java
@@ -1,7 +1,6 @@
 package com.enfernuz.quik.lua.rpc.serde.protobuf;
 
 import com.enfernuz.quik.lua.rpc.api.structures.DateTimeEntry;
-import com.enfernuz.quik.lua.rpc.serde.PbConverter;
 import com.enfernuz.quik.lua.rpc.serde.SerdeModule;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -15,7 +14,6 @@ import static org.junit.Assert.assertTrue;
 public class DateTimeEntryPbSerdeTest {
 
     private static SerdeModule sut;
-    private static PbConverter<QluaStructures.DateTimeEntry, DateTimeEntry> pbConverter;
 
     private static DateTimeEntry expectedObject;
     private static byte[] expectedPbInput;
@@ -24,7 +22,6 @@ public class DateTimeEntryPbSerdeTest {
     public static void globalSetup() {
 
         sut = ProtobufSerdeModule.INSTANCE;
-        pbConverter = DateTimeEntryPbSerde.INSTANCE;
 
         expectedObject = DateTimeEntry.builder()
                 .mcs(1)
@@ -37,7 +34,18 @@ public class DateTimeEntryPbSerdeTest {
                 .month(8)
                 .year(9)
                 .build();
-        expectedPbInput = pbConverter.convertToPb(expectedObject).toByteArray();
+        expectedPbInput = QluaStructures.DateTimeEntry.newBuilder()
+                .setMcs(1)
+                .setMs(2)
+                .setSec(3)
+                .setMin(4)
+                .setHour(5)
+                .setDay(6)
+                .setWeekDay(7)
+                .setMonth(8)
+                .setYear(9)
+                .build()
+                .toByteArray();
     }
 
     @Test
