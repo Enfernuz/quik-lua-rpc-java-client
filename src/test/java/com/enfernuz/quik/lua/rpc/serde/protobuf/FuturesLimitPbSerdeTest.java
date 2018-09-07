261f8d42e0b7344034b2605955f1e468cd406e6a
Arsentii Nerushev <asnerushev@gmail.com>
2018-09-07 23:08:28 +0300

    Removed a SUT's self-dependency, which improves the test's correctness.

--- src/test/java/com/enfernuz/quik/lua/rpc/serde/protobuf/FuturesLimitPbSerdeTest.java
+++ src/test/java/com/enfernuz/quik/lua/rpc/serde/protobuf/FuturesLimitPbSerdeTest.java
@@ -1,7 +1,6 @@
 package com.enfernuz.quik.lua.rpc.serde.protobuf;
 
 import com.enfernuz.quik.lua.rpc.api.structures.FuturesLimit;
-import com.enfernuz.quik.lua.rpc.serde.PbConverter;
 import com.enfernuz.quik.lua.rpc.serde.SerdeModule;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -15,7 +14,6 @@ import static org.junit.Assert.assertTrue;
 public class FuturesLimitPbSerdeTest {
 
     private static SerdeModule sut;
-    private static PbConverter<QluaStructures.FuturesLimit, FuturesLimit> pbConverter;
 
     private static FuturesLimit expectedObject;
     private static byte[] expectedPbInput;
@@ -27,7 +25,6 @@ public class FuturesLimitPbSerdeTest {
     public static void globalSetup() {
 
         sut = ProtobufSerdeModule.INSTANCE;
-        pbConverter = FuturesLimitPbSerde.INSTANCE;
 
         expectedObject = FuturesLimit.builder()
                 .firmId("1")
@@ -48,13 +45,34 @@ public class FuturesLimitPbSerdeTest {
                 .currCode("16")
                 .realVarMargin("17")
                 .build();
-        expectedPbInput = pbConverter.convertToPb(expectedObject).toByteArray();
+        expectedPbInput = QluaStructures.FuturesLimit.newBuilder()
+                .setFirmid("1")
+                .setTrdaccid("2")
+                .setLimitType(3)
+                .setLiquidityCoef("4")
+                .setCbpPrevLimit("5")
+                .setCbplimit("6")
+                .setCbplused("7")
+                .setCbplplanned("8")
+                .setVarmargin("9")
+                .setAccruedint("10")
+                .setCbplusedForOrders("11")
+                .setCbplusedForPositions("12")
+                .setOptionsPremium("13")
+                .setTsComission("14")
+                .setKgo("15")
+                .setCurrcode("16")
+                .setRealVarmargin("17")
+                .build()
+                .toByteArray();
 
         expectedObjectWithNullNonRequiredStringFileds = FuturesLimit.builder()
                 .limitType(1)
                 .build();
-        expectedPbInputWithEmptyNonRequiredStringFields =
-                pbConverter.convertToPb(expectedObjectWithNullNonRequiredStringFileds).toByteArray();
+        expectedPbInputWithEmptyNonRequiredStringFields = QluaStructures.FuturesLimit.newBuilder()
+                .setLimitType(1)
+                .build()
+                .toByteArray();
     }
 
     @Test
