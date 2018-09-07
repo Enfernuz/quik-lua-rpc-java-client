322a328830979ecf00608c8cb0328f6cdd4dbc18
Arsentii Nerushev <asnerushev@gmail.com>
2018-09-08 00:15:52 +0300

    Removed a SUT's self-dependency, which improves the test's correctness.

--- src/test/java/com/enfernuz/quik/lua/rpc/serde/protobuf/StopEventInfoPbSerdeTest.java
+++ src/test/java/com/enfernuz/quik/lua/rpc/serde/protobuf/StopEventInfoPbSerdeTest.java
@@ -1,7 +1,6 @@
 package com.enfernuz.quik.lua.rpc.serde.protobuf;
 
 import com.enfernuz.quik.lua.rpc.api.structures.StopEventInfo;
-import com.enfernuz.quik.lua.rpc.serde.PbConverter;
 import com.enfernuz.quik.lua.rpc.serde.SerdeModule;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -15,7 +14,6 @@ import static org.junit.Assert.assertTrue;
 public class StopEventInfoPbSerdeTest {
 
     private static SerdeModule sut;
-    private static PbConverter<QluaStructures.StopEventInfo, StopEventInfo> pbConverter;
 
     private static StopEventInfo expectedObject;
     private static byte[] expectedPbInput;
@@ -24,10 +22,12 @@ public class StopEventInfoPbSerdeTest {
     public static void globalSetup() {
 
         sut = ProtobufSerdeModule.INSTANCE;
-        pbConverter = StopEventInfoPbSerde.INSTANCE;
 
         expectedObject = new StopEventInfo(1);
-        expectedPbInput = pbConverter.convertToPb(expectedObject).toByteArray();
+        expectedPbInput = QluaStructures.StopEventInfo.newBuilder()
+                .setSignal(1)
+                .build()
+                .toByteArray();
     }
 
     @Test
