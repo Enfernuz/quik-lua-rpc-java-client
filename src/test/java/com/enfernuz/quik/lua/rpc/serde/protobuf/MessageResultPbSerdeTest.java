d28bcbba9b07021be1667e1d3e57eba1e079f097
Arsentii Nerushev <asnerushev@gmail.com>
2018-09-07 23:13:19 +0300

    Removed a SUT's self-dependency, which improves the test's correctness.

--- src/test/java/com/enfernuz/quik/lua/rpc/serde/protobuf/MessageResultPbSerdeTest.java
+++ src/test/java/com/enfernuz/quik/lua/rpc/serde/protobuf/MessageResultPbSerdeTest.java
@@ -1,7 +1,6 @@
 package com.enfernuz.quik.lua.rpc.serde.protobuf;
 
 import com.enfernuz.quik.lua.rpc.api.messages.Message;
-import com.enfernuz.quik.lua.rpc.serde.PbConverter;
 import com.enfernuz.quik.lua.rpc.serde.SerdeModule;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -14,7 +13,6 @@ import static org.junit.Assert.assertTrue;
 public class MessageResultPbSerdeTest {
 
     private static SerdeModule sut;
-    private static PbConverter<qlua.rpc.Message.Result, Message.Result> pbConverter;
 
     private static Message.Result expectedObject;
     private static byte[] expectedPbInput;
@@ -23,10 +21,12 @@ public class MessageResultPbSerdeTest {
     public static void globalSetup() {
 
         sut = ProtobufSerdeModule.INSTANCE;
-        pbConverter = MessageResultPbSerde.INSTANCE;
 
         expectedObject = new Message.Result(1);
-        expectedPbInput = pbConverter.convertToPb(expectedObject).toByteArray();
+        expectedPbInput = qlua.rpc.Message.Result.newBuilder()
+                .setResult(1)
+                .build()
+                .toByteArray();
     }
 
     @Test
