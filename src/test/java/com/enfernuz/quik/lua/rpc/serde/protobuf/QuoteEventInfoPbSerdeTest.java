ead011e8d83b0034fc8465c3e2825badcaeb43bc
Arsentii Nerushev <asnerushev@gmail.com>
2018-09-08 00:10:04 +0300

    Removed a SUT's self-dependency, which improves the test's correctness.

--- src/test/java/com/enfernuz/quik/lua/rpc/serde/protobuf/QuoteEventInfoPbSerdeTest.java
+++ src/test/java/com/enfernuz/quik/lua/rpc/serde/protobuf/QuoteEventInfoPbSerdeTest.java
@@ -1,7 +1,6 @@
 package com.enfernuz.quik.lua.rpc.serde.protobuf;
 
 import com.enfernuz.quik.lua.rpc.api.structures.QuoteEventInfo;
-import com.enfernuz.quik.lua.rpc.serde.PbConverter;
 import com.enfernuz.quik.lua.rpc.serde.SerdeModule;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -15,29 +14,30 @@ import static org.junit.Assert.assertTrue;
 public class QuoteEventInfoPbSerdeTest {
 
     private static SerdeModule sut;
-    private static PbConverter<QluaStructures.QuoteEventInfo, QuoteEventInfo> pbConverter;
 
     private static QuoteEventInfo expectedObject;
     private static byte[] expectedPbInput;
 
-    private static QuoteEventInfo expectedObjectWithNullNonRequiredStringFileds;
-    private static byte[] expectedPbInputWithEmptyNonRequiredStringFields;
+    private static QuoteEventInfo expectedObjectWithOnlyRequiredFields;
+    private static byte[] expectedPbInputWithOnlyRequiredFields;
 
     @BeforeClass
     public static void globalSetup() {
 
         sut = ProtobufSerdeModule.INSTANCE;
-        pbConverter = QuoteEventInfoPbSerde.INSTANCE;
 
         expectedObject = QuoteEventInfo.builder()
                 .classCode("1")
                 .secCode("2")
                 .build();
-        expectedPbInput = pbConverter.convertToPb(expectedObject).toByteArray();
+        expectedPbInput = QluaStructures.QuoteEventInfo.newBuilder()
+                .setClassCode("1")
+                .setSecCode("2")
+                .build()
+                .toByteArray();
 
-        expectedObjectWithNullNonRequiredStringFileds = QuoteEventInfo.builder().build();
-        expectedPbInputWithEmptyNonRequiredStringFields =
-                pbConverter.convertToPb(expectedObjectWithNullNonRequiredStringFileds).toByteArray();
+        expectedObjectWithOnlyRequiredFields = QuoteEventInfo.builder().build();
+        expectedPbInputWithOnlyRequiredFields = QluaStructures.QuoteEventInfo.newBuilder().build().toByteArray();
     }
 
     @Test
@@ -51,9 +51,9 @@ public class QuoteEventInfoPbSerdeTest {
     @Test
     public void testSerializePbInputWithEmptyNonRequiredStringFields() {
 
-        final byte[] actual = sut.serialize(expectedObjectWithNullNonRequiredStringFileds);
+        final byte[] actual = sut.serialize(expectedObjectWithOnlyRequiredFields);
 
-        assertTrue( Arrays.equals(expectedPbInputWithEmptyNonRequiredStringFields, actual) );
+        assertTrue( Arrays.equals(expectedPbInputWithOnlyRequiredFields, actual) );
     }
 
     @Test
@@ -67,8 +67,8 @@ public class QuoteEventInfoPbSerdeTest {
     @Test
     public void testDeserializePbInputWithEmptyNonRequiredStringFields() {
 
-        final QuoteEventInfo actualObject = sut.deserialize(QuoteEventInfo.class, expectedPbInputWithEmptyNonRequiredStringFields);
+        final QuoteEventInfo actualObject = sut.deserialize(QuoteEventInfo.class, expectedPbInputWithOnlyRequiredFields);
 
-        assertEquals(actualObject, expectedObjectWithNullNonRequiredStringFileds);
+        assertEquals(actualObject, expectedObjectWithOnlyRequiredFields);
     }
 }
