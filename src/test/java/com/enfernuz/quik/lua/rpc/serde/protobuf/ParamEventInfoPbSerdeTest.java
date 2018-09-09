e8c46c7a528011cd64ff012923c788a6c34c0ff1
Arsentii Nerushev <asnerushev@gmail.com>
2018-09-09 22:37:38 +0300

    Renamed some fields and methods, formatted the code; added ser/deser test cases for object with only the required fields set.

--- src/test/java/com/enfernuz/quik/lua/rpc/serde/protobuf/ParamEventInfoPbSerdeTest.java
+++ src/test/java/com/enfernuz/quik/lua/rpc/serde/protobuf/ParamEventInfoPbSerdeTest.java
@@ -43,32 +43,33 @@ public class ParamEventInfoPbSerdeTest {
     @Test
     public void testSerialize() {
 
-        final byte[] actual = sut.serialize(expectedObject);
-
-        assertTrue( Arrays.equals(expectedPbInput, actual) );
-    }
-
-    @Test
-    public void testSerializePbInputWithEmptyNonRequiredStringFields() {
-
-        final byte[] actual = sut.serialize(expectedObjectWithOnlyRequiredFields);
-
-        assertTrue( Arrays.equals(expectedPbInputWithOnlyRequiredFields, actual) );
+        assertTrue(
+                Arrays.equals(expectedPbInput, sut.serialize(expectedObject))
+        );
     }
 
     @Test
     public void testDeserialize() {
-
-        final ParamEventInfo actualObject = sut.deserialize(ParamEventInfo.class, expectedPbInput);
-
-        assertEquals(actualObject, expectedObject);
+        assertEquals(expectedObject, sut.deserialize(ParamEventInfo.class, expectedPbInput));
     }
 
     @Test
-    public void testDeserializePbInputWithEmptyNonRequiredStringFields() {
+    public void testSerialize_WithOnlyRequiredFields() {
 
-        final ParamEventInfo actualObject = sut.deserialize(ParamEventInfo.class, expectedPbInputWithOnlyRequiredFields);
+        assertTrue(
+                Arrays.equals(
+                        expectedPbInputWithOnlyRequiredFields,
+                        sut.serialize(expectedObjectWithOnlyRequiredFields)
+                )
+        );
+    }
 
-        assertEquals(actualObject, expectedObjectWithOnlyRequiredFields);
+    @Test
+    public void testDeserialize_WithOnlyRequiredFields() {
+
+        assertEquals(
+                expectedObjectWithOnlyRequiredFields,
+                sut.deserialize(ParamEventInfo.class, expectedPbInputWithOnlyRequiredFields)
+        );
     }
 }
