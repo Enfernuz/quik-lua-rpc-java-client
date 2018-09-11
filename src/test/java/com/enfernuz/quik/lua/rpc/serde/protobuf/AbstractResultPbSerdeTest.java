715cd0ab4111dff12ad6322423ceaafda741dcc6
Arsentii Nerushev <asnerushev@gmail.com>
2018-09-12 02:38:57 +0300

    Added an abstract test class for testing ser/deser of the *Result classes.

---
+++ src/test/java/com/enfernuz/quik/lua/rpc/serde/protobuf/AbstractResultPbSerdeTest.java
@@ -0,0 +1,6 @@
+package com.enfernuz.quik.lua.rpc.serde.protobuf;
+
+import com.google.protobuf.MessageLite;
+
+public abstract class AbstractResultPbSerdeTest<TARGET_OBJECT, PB_OBJECT extends MessageLite> extends AbstractPbSerdeTest<TARGET_OBJECT, PB_OBJECT> {
+}
