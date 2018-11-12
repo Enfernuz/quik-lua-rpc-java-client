15e9ceab48d595470fac460bb37776e5ebfbc2a4
Arsentii Nerushev <asnerushev@gmail.com>
2018-11-12 23:55:55 +0300

    Rewrote test for GetQuoteLevel2.Args protobuf serialization.

--- src/test/java/com/enfernuz/quik/lua/rpc/serde/protobuf/GetQuoteLevel2RequestPbSerdeTest.java
+++ src/test/java/com/enfernuz/quik/lua/rpc/serde/protobuf/GetQuoteLevel2RequestPbSerdeTest.java
@@ -1,38 +1,34 @@
 package com.enfernuz.quik.lua.rpc.serde.protobuf;
 
 import com.enfernuz.quik.lua.rpc.api.messages.GetQuoteLevel2;
-import com.google.protobuf.MessageLite;
+import com.enfernuz.quik.lua.rpc.serde.Serializer;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-import qlua.rpc.RPC;
 
-public class GetQuoteLevel2RequestPbSerdeTest extends AbstractRequestPbSerdeTest<GetQuoteLevel2.Request> {
+public class GetQuoteLevel2RequestPbSerdeTest extends AbstractPbSerializationTest<GetQuoteLevel2.Args, qlua.rpc.GetQuoteLevel2.Request> {
 
     private static final String CLASS_CODE = "1";
     private static final String SEC_CODE = "2";
 
     @Override
-    public RPC.@NotNull ProcedureType getProcedureType() {
-        return RPC.ProcedureType.GET_QUOTE_LEVEL2;
+    public @NotNull Serializer<GetQuoteLevel2.Args> getSerializerUnderTest() {
+        return GetQuoteLevel2ArgsPbSerializer.INSTANCE;
     }
 
+    @NotNull
     @Override
-    public @Nullable MessageLite getPbRequestArgs() {
+    public qlua.rpc.GetQuoteLevel2.Request getTargetObjectAsPbMessage() {
+
         return qlua.rpc.GetQuoteLevel2.Request.newBuilder()
                 .setClassCode(CLASS_CODE)
                 .setSecCode(SEC_CODE)
                 .build();
     }
 
-    @Override
-    public @NotNull Class<GetQuoteLevel2.Request> getTargetObjectClass() {
-        return GetQuoteLevel2.Request.class;
-    }
-
     @NotNull
     @Override
-    public GetQuoteLevel2.Request getTargetObject() {
-        return GetQuoteLevel2.Request.builder()
+    public GetQuoteLevel2.Args getTargetObject() {
+
+        return GetQuoteLevel2.Args.builder()
                 .classCode(CLASS_CODE)
                 .secCode(SEC_CODE)
                 .build();
