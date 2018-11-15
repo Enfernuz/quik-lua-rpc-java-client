dbf1a3b01158eec6e0a366677e83fb0c95c36ec6
Arsentii Nerushev <asnerushev@gmail.com>
2018-11-16 01:27:27 +0300

    Rewrote test for GetFuturesHolding.Result protobuf deserialization.

--- src/test/java/com/enfernuz/quik/lua/rpc/serde/protobuf/GetFuturesHoldingResultPbSerdeTest.java
+++ src/test/java/com/enfernuz/quik/lua/rpc/serde/protobuf/GetFuturesHoldingResultPbSerdeTest.java
@@ -2,114 +2,95 @@ package com.enfernuz.quik.lua.rpc.serde.protobuf;
 
 import com.enfernuz.quik.lua.rpc.api.messages.GetFuturesHolding;
 import com.enfernuz.quik.lua.rpc.api.structures.FuturesClientHolding;
-import com.enfernuz.quik.lua.rpc.serde.SerdeModule;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import com.enfernuz.quik.lua.rpc.serde.Deserializer;
+import org.jetbrains.annotations.NotNull;
 import qlua.structs.QluaStructures;
 
-import java.util.Arrays;
+public class GetFuturesHoldingResultPbSerdeTest extends AbstractPbDeserializationTest<qlua.rpc.GetFuturesHolding.Result, GetFuturesHolding.Result> {
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+    private static final String FIRM_ID = "1";
+    private static final String TRD_ACC_ID = "2";
+    private static final String SEC_CODE = "3";
+    private static final int TYPE = 4;
+    private static final String START_BUY = "5";
+    private static final String START_SELL = "6";
+    private static final String TODAY_BUY = "7";
+    private static final String TODAY_SELL = "8";
+    private static final String TOTAL_NET = "9";
+    private static final int OPEN_BUYS = 10;
+    private static final int OPEN_SELLS = 11;
+    private static final String CBPL_USED = "12";
+    private static final String CBPL_PLANNED = "13";
+    private static final String VAR_MARGIN = "14";
+    private static final String AVR_POSN_PRICE = "15";
+    private static final String POSITION_VALUE = "16";
+    private static final String REAL_VAR_MARGIN = "17";
+    private static final String TOTAL_VAR_MARGIN = "18";
+    private static final int SESSION_STATUS = 19;
 
-public class GetFuturesHoldingResultPbSerdeTest {
 
-    private static SerdeModule sut;
+    @Override
+    public @NotNull Deserializer<GetFuturesHolding.Result> getDeserializerUnderTest() {
+        return GetFuturesHoldingResultPbDeserializer.INSTANCE;
+    }
 
-    private static GetFuturesHolding.Result expectedObject;
-    private static byte[] expectedPbInput;
-
-    private static GetFuturesHolding.Result expectedObjectWithOnlyRequiredFields;
-    private static byte[] expectedPbInputWithOnlyRequiredFields;
-
-    @BeforeClass
-    public static void globalSetup() {
-
-        sut = ProtobufSerdeModule.INSTANCE;
+    @NotNull
+    @Override
+    public GetFuturesHolding.Result getTargetObject() {
 
         final FuturesClientHolding futuresClientHolding = FuturesClientHolding.builder()
-                .firmId("1")
-                .trdAccId("2")
-                .secCode("3")
-                .type(4)
-                .startBuy("5")
-                .startSell("6")
-                .todayBuy("7")
-                .todaySell("8")
-                .totalNet("9")
-                .openBuys(10)
-                .openSells(11)
-                .cbplUsed("12")
-                .cbplPlanned("13")
-                .varMargin("14")
-                .avrPosnPrice("15")
-                .positionValue("16")
-                .realVarMargin("17")
-                .totalVarMargin("18")
-                .sessionStatus(19)
+                .firmId(FIRM_ID)
+                .trdAccId(TRD_ACC_ID)
+                .secCode(SEC_CODE)
+                .type(TYPE)
+                .startBuy(START_BUY)
+                .startSell(START_SELL)
+                .todayBuy(TODAY_BUY)
+                .todaySell(TODAY_SELL)
+                .totalNet(TOTAL_NET)
+                .openBuys(OPEN_BUYS)
+                .openSells(OPEN_SELLS)
+                .cbplUsed(CBPL_USED)
+                .cbplPlanned(CBPL_PLANNED)
+                .varMargin(VAR_MARGIN)
+                .avrPosnPrice(AVR_POSN_PRICE)
+                .positionValue(POSITION_VALUE)
+                .realVarMargin(REAL_VAR_MARGIN)
+                .totalVarMargin(TOTAL_VAR_MARGIN)
+                .sessionStatus(SESSION_STATUS)
                 .build();
+
+        return GetFuturesHolding.Result.getInstance(futuresClientHolding);
+    }
+
+    @NotNull
+    @Override
+    public qlua.rpc.GetFuturesHolding.Result getTargetObjectAsPbMessage() {
+
         final QluaStructures.FuturesClientHolding pbFuturesClientHolding = QluaStructures.FuturesClientHolding.newBuilder()
-                .setFirmid("1")
-                .setTrdaccid("2")
-                .setSecCode("3")
-                .setType(4)
-                .setStartbuy("5")
-                .setStartsell("6")
-                .setTodaybuy("7")
-                .setTodaysell("8")
-                .setTotalnet("9")
-                .setOpenbuys(10)
-                .setOpensells(11)
-                .setCbplused("12")
-                .setCbplplanned("13")
-                .setVarmargin("14")
-                .setAvrposnprice("15")
-                .setPositionvalue("16")
-                .setRealVarmargin("17")
-                .setTotalVarmargin("18")
-                .setSessionStatus(19)
+                .setFirmid(FIRM_ID)
+                .setTrdaccid(TRD_ACC_ID)
+                .setSecCode(SEC_CODE)
+                .setType(TYPE)
+                .setStartbuy(START_BUY)
+                .setStartsell(START_SELL)
+                .setTodaybuy(TODAY_BUY)
+                .setTodaysell(TODAY_SELL)
+                .setTotalnet(TOTAL_NET)
+                .setOpenbuys(OPEN_BUYS)
+                .setOpensells(OPEN_SELLS)
+                .setCbplused(CBPL_USED)
+                .setCbplplanned(CBPL_PLANNED)
+                .setVarmargin(VAR_MARGIN)
+                .setAvrposnprice(AVR_POSN_PRICE)
+                .setPositionvalue(POSITION_VALUE)
+                .setRealVarmargin(REAL_VAR_MARGIN)
+                .setTotalVarmargin(TOTAL_VAR_MARGIN)
+                .setSessionStatus(SESSION_STATUS)
                 .build();
 
-        expectedObject = GetFuturesHolding.Result.getInstance(futuresClientHolding);
-        expectedPbInput = qlua.rpc.GetFuturesHolding.Result.newBuilder()
+        return qlua.rpc.GetFuturesHolding.Result.newBuilder()
                 .setFuturesHolding(pbFuturesClientHolding)
-                .build()
-                .toByteArray();
-
-        expectedObjectWithOnlyRequiredFields = GetFuturesHolding.Result.getInstance(null);
-        expectedPbInputWithOnlyRequiredFields = qlua.rpc.GetFuturesHolding.Result.newBuilder().build().toByteArray();
-    }
-
-    @Test
-    public void testSerialize() {
-
-        assertTrue(
-                Arrays.equals(expectedPbInput, sut.serialize(expectedObject))
-        );
-    }
-
-    @Test
-    public void testDeserialize() {
-        assertEquals(expectedObject, sut.deserialize(GetFuturesHolding.Result.class, expectedPbInput));
-    }
-
-    @Test
-    public void testSerialize_WithOnlyRequiredFields() {
-
-        assertTrue(
-                Arrays.equals(
-                        expectedPbInputWithOnlyRequiredFields,
-                        sut.serialize(expectedObjectWithOnlyRequiredFields)
-                )
-        );
-    }
-
-    @Test
-    public void testDeserialize_WithOnlyRequiredFields() {
-
-        assertEquals(
-                expectedObjectWithOnlyRequiredFields,
-                sut.deserialize(GetFuturesHolding.Result.class, expectedPbInputWithOnlyRequiredFields)
-        );
+                .build();
     }
 }
