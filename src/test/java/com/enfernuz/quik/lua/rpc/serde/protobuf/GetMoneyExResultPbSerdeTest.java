eb265255688b6e0f4f42840447f0fd260f1b4f22
Arsentii Nerushev <asnerushev@gmail.com>
2018-11-16 01:28:12 +0300

    Rewrote test for GetMoneyEx.Result protobuf deserialization.

--- src/test/java/com/enfernuz/quik/lua/rpc/serde/protobuf/GetMoneyExResultPbSerdeTest.java
+++ src/test/java/com/enfernuz/quik/lua/rpc/serde/protobuf/GetMoneyExResultPbSerdeTest.java
@@ -2,11 +2,14 @@ package com.enfernuz.quik.lua.rpc.serde.protobuf;
 
 import com.enfernuz.quik.lua.rpc.api.messages.GetMoneyEx;
 import com.enfernuz.quik.lua.rpc.api.structures.MoneyLimit;
+import com.enfernuz.quik.lua.rpc.serde.Deserializer;
 import org.jetbrains.annotations.NotNull;
-import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
 import qlua.structs.QluaStructures;
 
-public class GetMoneyExResultPbSerdeTest extends AbstractResultPbSerdeTest<GetMoneyEx.Result, qlua.rpc.GetMoneyEx.Result> {
+@RunWith(Enclosed.class)
+public class GetMoneyExResultPbSerdeTest  {
 
     private static final String CURR_CODE = "1";
     private static final String TAG = "2";
@@ -22,80 +25,79 @@ public class GetMoneyExResultPbSerdeTest extends AbstractResultPbSerdeTest<GetMo
     private static final String LEVERAGE = "12";
     private static final int LIMIT_KIND = 13;
 
-    @Test
-    @Override
-    public void shouldSerialize_ErrorObject_To_ByteArrayOf_PbErrorObject() {
-        super.shouldSerialize_ErrorObject_To_ByteArrayOf_PbErrorObject();
+    public static class NormalGetMoneyExResultPbSerdeTest extends AbstractPbDeserializationTest<qlua.rpc.GetMoneyEx.Result, GetMoneyEx.Result> {
+
+        @Override
+        public @NotNull Deserializer<GetMoneyEx.Result> getDeserializerUnderTest() {
+            return GetMoneyExResultPbDeserializer.INSTANCE;
+        }
+
+        @NotNull
+        @Override
+        public GetMoneyEx.Result getTargetObject() {
+
+            final MoneyLimit moneyLimit = MoneyLimit.builder()
+                    .currCode(CURR_CODE)
+                    .tag(TAG)
+                    .firmId(FIRM_ID)
+                    .clientCode(CLIENT_CODE)
+                    .openBal(OPEN_BAL)
+                    .openLimit(OPEN_LIMIT)
+                    .currentBal(CURRENT_BAL)
+                    .currentLimit(CURRENT_LIMIT)
+                    .locked(LOCKED)
+                    .lockedValueCoef(LOCKED_VALUE_COEF)
+                    .lockedMarginValue(LOCKED_MARGIN_VALUE)
+                    .leverage(LEVERAGE)
+                    .limitKind(LIMIT_KIND)
+                    .build();
+
+            return GetMoneyEx.Result.getInstance(moneyLimit);
+        }
+
+        @NotNull
+        @Override
+        public qlua.rpc.GetMoneyEx.Result getTargetObjectAsPbMessage() {
+
+            final QluaStructures.MoneyLimit moneyLimit = QluaStructures.MoneyLimit.newBuilder()
+                    .setCurrcode(CURR_CODE)
+                    .setTag(TAG)
+                    .setFirmid(FIRM_ID)
+                    .setClientCode(CLIENT_CODE)
+                    .setOpenbal(OPEN_BAL)
+                    .setOpenlimit(OPEN_LIMIT)
+                    .setCurrentbal(CURRENT_BAL)
+                    .setCurrentlimit(CURRENT_LIMIT)
+                    .setLocked(LOCKED)
+                    .setLockedValueCoef(LOCKED_VALUE_COEF)
+                    .setLockedMarginValue(LOCKED_MARGIN_VALUE)
+                    .setLeverage(LEVERAGE)
+                    .setLimitKind(LIMIT_KIND)
+                    .build();
+
+            return qlua.rpc.GetMoneyEx.Result.newBuilder()
+                    .setMoneyEx(moneyLimit)
+                    .build();
+        }
     }
 
-    @Test
-    @Override
-    public void shouldDeserialize_ByteArrayOf_PbErrorObject_To_ErrorObject() {
-        super.shouldDeserialize_ByteArrayOf_PbErrorObject_To_ErrorObject();
-    }
+    public static class ErrorGetMoneyExResultPbSerdeTest extends AbstractPbDeserializationTest<qlua.rpc.GetMoneyEx.Result, GetMoneyEx.Result> {
 
-    @Override
-    public @NotNull Class<GetMoneyEx.Result> getTargetObjectClass() {
-        return GetMoneyEx.Result.class;
-    }
+        @Override
+        public @NotNull Deserializer<GetMoneyEx.Result> getDeserializerUnderTest() {
+            return GetMoneyExResultPbDeserializer.INSTANCE;
+        }
 
-    @NotNull
-    @Override
-    public qlua.rpc.GetMoneyEx.Result getTargetObjectAsPbMessage() {
+        @NotNull
+        @Override
+        public GetMoneyEx.Result getTargetObject() {
+            return GetMoneyEx.Result.getInstance(null);
+        }
 
-        final QluaStructures.MoneyLimit moneyLimit = QluaStructures.MoneyLimit.newBuilder()
-                .setCurrcode(CURR_CODE)
-                .setTag(TAG)
-                .setFirmid(FIRM_ID)
-                .setClientCode(CLIENT_CODE)
-                .setOpenbal(OPEN_BAL)
-                .setOpenlimit(OPEN_LIMIT)
-                .setCurrentbal(CURRENT_BAL)
-                .setCurrentlimit(CURRENT_LIMIT)
-                .setLocked(LOCKED)
-                .setLockedValueCoef(LOCKED_VALUE_COEF)
-                .setLockedMarginValue(LOCKED_MARGIN_VALUE)
-                .setLeverage(LEVERAGE)
-                .setLimitKind(LIMIT_KIND)
-                .build();
-
-        return qlua.rpc.GetMoneyEx.Result.newBuilder()
-                .setMoneyEx(moneyLimit)
-                .build();
-    }
-
-    @NotNull
-    @Override
-    public GetMoneyEx.Result getTargetObject() {
-
-        final MoneyLimit moneyLimit = MoneyLimit.builder()
-                .currCode(CURR_CODE)
-                .tag(TAG)
-                .firmId(FIRM_ID)
-                .clientCode(CLIENT_CODE)
-                .openBal(OPEN_BAL)
-                .openLimit(OPEN_LIMIT)
-                .currentBal(CURRENT_BAL)
-                .currentLimit(CURRENT_LIMIT)
-                .locked(LOCKED)
-                .lockedValueCoef(LOCKED_VALUE_COEF)
-                .lockedMarginValue(LOCKED_MARGIN_VALUE)
-                .leverage(LEVERAGE)
-                .limitKind(LIMIT_KIND)
-                .build();
-
-        return GetMoneyEx.Result.getInstance(moneyLimit);
-    }
-
-    @NotNull
-    @Override
-    public qlua.rpc.GetMoneyEx.Result getErrorObject_AsPbMessage() {
-        return qlua.rpc.GetMoneyEx.Result.newBuilder().build();
-    }
-
-    @NotNull
-    @Override
-    public GetMoneyEx.Result getErrorObject() {
-        return GetMoneyEx.Result.getInstance(null);
+        @NotNull
+        @Override
+        public qlua.rpc.GetMoneyEx.Result getTargetObjectAsPbMessage() {
+            return qlua.rpc.GetMoneyEx.Result.newBuilder().build();
+        }
     }
 }
