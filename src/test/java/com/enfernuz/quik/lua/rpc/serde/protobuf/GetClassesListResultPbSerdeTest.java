1a21e35e1a7161e87db7d0eb6c1fad3e89dc41ae
Arsentii Nerushev <asnerushev@gmail.com>
2018-11-16 01:26:59 +0300

    Rewrote test for GetClassesList.Result protobuf deserialization.

--- src/test/java/com/enfernuz/quik/lua/rpc/serde/protobuf/GetClassesListResultPbSerdeTest.java
+++ src/test/java/com/enfernuz/quik/lua/rpc/serde/protobuf/GetClassesListResultPbSerdeTest.java
@@ -1,69 +1,30 @@
 package com.enfernuz.quik.lua.rpc.serde.protobuf;
 
 import com.enfernuz.quik.lua.rpc.api.messages.GetClassesList;
-import com.enfernuz.quik.lua.rpc.serde.SerdeModule;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import com.enfernuz.quik.lua.rpc.serde.Deserializer;
+import org.jetbrains.annotations.NotNull;
 
-import java.util.Arrays;
+public class GetClassesListResultPbSerdeTest extends AbstractPbDeserializationTest<qlua.rpc.GetClassesList.Result, GetClassesList.Result> {
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+    private static final String CLASSES_LIST = "1";
 
-public class GetClassesListResultPbSerdeTest {
-
-    private static SerdeModule sut;
-
-    private static GetClassesList.Result expectedObject;
-    private static byte[] expectedPbInput;
-
-    private static GetClassesList.Result expectedObjectWithOnlyRequiredFields;
-    private static byte[] expectedPbInputWithOnlyRequiredFields;
-
-    @BeforeClass
-    public static void globalSetup() {
-
-        sut = ProtobufSerdeModule.INSTANCE;
-
-        expectedObject = new GetClassesList.Result("1");
-        expectedPbInput = qlua.rpc.GetClassesList.Result.newBuilder()
-                .setClassesList("1")
-                .build()
-                .toByteArray();
-
-        expectedObjectWithOnlyRequiredFields = new GetClassesList.Result(null);
-        expectedPbInputWithOnlyRequiredFields = qlua.rpc.GetClassesList.Result.newBuilder().build().toByteArray();
+    @Override
+    public @NotNull Deserializer<GetClassesList.Result> getDeserializerUnderTest() {
+        return GetClassesListResultPbDeserializer.INSTANCE;
     }
 
-    @Test
-    public void testSerialize() {
-
-        final byte[] actual = sut.serialize(expectedObject);
-
-        assertTrue( Arrays.equals(expectedPbInput, actual) );
+    @NotNull
+    @Override
+    public GetClassesList.Result getTargetObject() {
+        return new GetClassesList.Result(CLASSES_LIST);
     }
 
-    @Test
-    public void testDeserialize() {
+    @NotNull
+    @Override
+    public qlua.rpc.GetClassesList.Result getTargetObjectAsPbMessage() {
 
-        final GetClassesList.Result actualObject = sut.deserialize(GetClassesList.Result.class, expectedPbInput);
-
-        assertEquals(actualObject, expectedObject);
-    }
-
-    @Test
-    public void testSerializeWithOnlyRequiredFields() {
-
-        final byte[] actual = sut.serialize(expectedObjectWithOnlyRequiredFields);
-
-        assertTrue( Arrays.equals(expectedPbInputWithOnlyRequiredFields, actual) );
-    }
-
-    @Test
-    public void testDeserializeWithOnlyRequiredFields() {
-
-        final GetClassesList.Result actualObject = sut.deserialize(GetClassesList.Result.class, expectedPbInputWithOnlyRequiredFields);
-
-        assertEquals(actualObject, expectedObjectWithOnlyRequiredFields);
+        return qlua.rpc.GetClassesList.Result.newBuilder()
+                .setClassesList(CLASSES_LIST)
+                .build();
     }
 }
