ac3af4074ac89c73f2b0d95511610381f3a53a08
Arsentii Nerushev <asnerushev@gmail.com>
2018-11-16 01:28:29 +0300

    Rewrote test for GetMoney.Result protobuf deserialization.

--- src/test/java/com/enfernuz/quik/lua/rpc/serde/protobuf/GetMoneyResultPbSerdeTest.java
+++ src/test/java/com/enfernuz/quik/lua/rpc/serde/protobuf/GetMoneyResultPbSerdeTest.java
@@ -2,9 +2,10 @@ package com.enfernuz.quik.lua.rpc.serde.protobuf;
 
 import com.enfernuz.quik.lua.rpc.api.messages.GetMoney;
 import com.enfernuz.quik.lua.rpc.api.structures.Money;
+import com.enfernuz.quik.lua.rpc.serde.Deserializer;
 import org.jetbrains.annotations.NotNull;
 
-public class GetMoneyResultPbSerdeTest extends AbstractResultPbSerdeTest<GetMoney.Result, qlua.rpc.GetMoney.Result> {
+public class GetMoneyResultPbSerdeTest extends AbstractPbDeserializationTest<qlua.rpc.GetMoney.Result, GetMoney.Result> {
 
     private static final String MONEY_OPEN_LIMIT = "1";
     private static final String MONEY_LIMIT_LOCKED_NON_MARGINAL_VALUE = "2";
@@ -14,10 +15,26 @@ public class GetMoneyResultPbSerdeTest extends AbstractResultPbSerdeTest<GetMone
     private static final String MONEY_CURRENT_BALANCE = "6";
     private static final String MONEY_LIMIT_AVAILABLE = "7";
 
+    @Override
+    public @NotNull Deserializer<GetMoney.Result> getDeserializerUnderTest() {
+        return GetMoneyResultPbDeserializer.INSTANCE;
+    }
+
     @NotNull
     @Override
-    public Class<GetMoney.Result> getTargetObjectClass() {
-        return GetMoney.Result.class;
+    public GetMoney.Result getTargetObject() {
+
+        final Money money = Money.builder()
+                .moneyOpenLimit(MONEY_OPEN_LIMIT)
+                .moneyLimitLockedNonMarginalValue(MONEY_LIMIT_LOCKED_NON_MARGINAL_VALUE)
+                .moneyLimitLocked(MONEY_LIMIT_LOCKED)
+                .moneyOpenBalance(MONEY_OPEN_BALANCE)
+                .moneyCurrentLimit(MONEY_CURRENT_LIMIT)
+                .moneyCurrentBalance(MONEY_CURRENT_BALANCE)
+                .moneyLimitAvailable(MONEY_LIMIT_AVAILABLE)
+                .build();
+
+        return new GetMoney.Result(money);
     }
 
     @NotNull
@@ -37,22 +54,5 @@ public class GetMoneyResultPbSerdeTest extends AbstractResultPbSerdeTest<GetMone
         return qlua.rpc.GetMoney.Result.newBuilder()
                 .setMoney(money)
                 .build();
-    }
-
-    @NotNull
-    @Override
-    public GetMoney.Result getTargetObject() {
-
-        final Money money = Money.builder()
-                .moneyOpenLimit(MONEY_OPEN_LIMIT)
-                .moneyLimitLockedNonMarginalValue(MONEY_LIMIT_LOCKED_NON_MARGINAL_VALUE)
-                .moneyLimitLocked(MONEY_LIMIT_LOCKED)
-                .moneyOpenBalance(MONEY_OPEN_BALANCE)
-                .moneyCurrentLimit(MONEY_CURRENT_LIMIT)
-                .moneyCurrentBalance(MONEY_CURRENT_BALANCE)
-                .moneyLimitAvailable(MONEY_LIMIT_AVAILABLE)
-                .build();
-
-        return new GetMoney.Result(money);
     }
 }
