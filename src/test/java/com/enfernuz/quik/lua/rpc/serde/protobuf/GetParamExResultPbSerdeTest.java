ca91b0a7af5043cd29f59f355800528289b5d893
Arsentii Nerushev <asnerushev@gmail.com>
2018-11-16 01:29:12 +0300

    Rewrote test for GetParamEx.Result protobuf deserialization.

--- src/test/java/com/enfernuz/quik/lua/rpc/serde/protobuf/GetParamExResultPbSerdeTest.java
+++ src/test/java/com/enfernuz/quik/lua/rpc/serde/protobuf/GetParamExResultPbSerdeTest.java
@@ -1,19 +1,15 @@
 package com.enfernuz.quik.lua.rpc.serde.protobuf;
 
 import com.enfernuz.quik.lua.rpc.api.messages.GetParamEx;
-import com.enfernuz.quik.lua.rpc.serde.SerdeException;
-import com.enfernuz.quik.lua.rpc.serde.protobuf.AbstractResultPbSerdeTest;
+import com.enfernuz.quik.lua.rpc.serde.Deserializer;
 import org.jetbrains.annotations.NotNull;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
-import static org.hamcrest.CoreMatchers.allOf;
 import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.Matchers.hasProperty;
-import static org.hamcrest.Matchers.instanceOf;
 
-public class GetParamExResultPbSerdeTest extends AbstractResultPbSerdeTest<GetParamEx.Result, qlua.rpc.GetParamEx.Result> {
+public class GetParamExResultPbSerdeTest extends AbstractPbDeserializationTest<qlua.rpc.GetParamEx.Result, GetParamEx.Result> {
 
     private static final String PARAM_TYPE = "1";
     private static final String PARAM_VALUE = "2";
@@ -24,8 +20,22 @@ public class GetParamExResultPbSerdeTest extends AbstractResultPbSerdeTest<GetPa
     public ExpectedException thrown = ExpectedException.none();
 
     @Override
-    public @NotNull Class<GetParamEx.Result> getTargetObjectClass() {
-        return GetParamEx.Result.class;
+    public @NotNull Deserializer<GetParamEx.Result> getDeserializerUnderTest() {
+        return GetParamExResultPbDeserializer.INSTANCE;
+    }
+
+    @NotNull
+    @Override
+    public GetParamEx.Result getTargetObject() {
+
+        final GetParamEx.ParamEx paramEx = GetParamEx.ParamEx.builder()
+                .paramType(PARAM_TYPE)
+                .paramValue(PARAM_VALUE)
+                .paramImage(PARAM_IMAGE)
+                .result(RESULT)
+                .build();
+
+        return new GetParamEx.Result(paramEx);
     }
 
     @NotNull
@@ -44,31 +54,15 @@ public class GetParamExResultPbSerdeTest extends AbstractResultPbSerdeTest<GetPa
                 .build();
     }
 
-    @NotNull
-    @Override
-    public GetParamEx.Result getTargetObject() {
-
-        final GetParamEx.ParamEx paramEx = GetParamEx.ParamEx.builder()
-                .paramType(PARAM_TYPE)
-                .paramValue(PARAM_VALUE)
-                .paramImage(PARAM_IMAGE)
-                .result(RESULT)
-                .build();
-
-        return new GetParamEx.Result(paramEx);
-    }
-
+    // TODO: really shouldn't be a responsibility of the deserializer
     @Test
     public void shouldFailOnUninitializedPbParamEx() {
 
-        thrown.expect(SerdeException.class);
-        thrown.expectCause(
-                allOf(
-                        instanceOf(IllegalArgumentException.class),
-                        hasProperty("message", equalTo("Поле 'param_ex' экземпляра protobuf-представления qlua.rpc.GetParamEx.Result не инициализировано."))
-                )
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage(
+                equalTo("Поле 'param_ex' экземпляра protobuf-представления qlua.rpc.GetParamEx.Result не инициализировано.")
         );
 
-        getSerdeModuleUnderTest().deserialize(GetParamEx.Result.class, qlua.rpc.GetParamEx.Result.newBuilder().build().toByteArray());
+        getDeserializerUnderTest().deserialize(qlua.rpc.GetParamEx.Result.newBuilder().build().toByteArray());
     }
 }
