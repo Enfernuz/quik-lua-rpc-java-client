82472b6778ef8d88895c818b8e7f69d06c7f7462
Arsentii Nerushev <asnerushev@gmail.com>
2018-11-16 01:29:26 +0300

    Rewrote test for GetTradeDate.Result protobuf deserialization.

--- src/test/java/com/enfernuz/quik/lua/rpc/serde/protobuf/GetTradeDateResultPbSerdeTest.java
+++ src/test/java/com/enfernuz/quik/lua/rpc/serde/protobuf/GetTradeDateResultPbSerdeTest.java
@@ -1,18 +1,15 @@
 package com.enfernuz.quik.lua.rpc.serde.protobuf;
 
 import com.enfernuz.quik.lua.rpc.api.messages.GetTradeDate;
-import com.enfernuz.quik.lua.rpc.serde.SerdeException;
+import com.enfernuz.quik.lua.rpc.serde.Deserializer;
 import org.jetbrains.annotations.NotNull;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
-import static org.hamcrest.CoreMatchers.allOf;
 import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.Matchers.hasProperty;
-import static org.hamcrest.Matchers.instanceOf;
 
-public class GetTradeDateResultPbSerdeTest extends AbstractResultPbSerdeTest<GetTradeDate.Result, qlua.rpc.GetTradeDate.Result> {
+public class GetTradeDateResultPbSerdeTest extends AbstractPbDeserializationTest<qlua.rpc.GetTradeDate.Result, GetTradeDate.Result> {
 
     private static final String DATE = "1";
     private static final int YEAR = 2;
@@ -23,8 +20,22 @@ public class GetTradeDateResultPbSerdeTest extends AbstractResultPbSerdeTest<Get
     public ExpectedException thrown = ExpectedException.none();
 
     @Override
-    public @NotNull Class<GetTradeDate.Result> getTargetObjectClass() {
-        return GetTradeDate.Result.class;
+    public @NotNull Deserializer<GetTradeDate.Result> getDeserializerUnderTest() {
+        return GetTradeDateResultPbDeserializer.INSTANCE;
+    }
+
+    @NotNull
+    @Override
+    public GetTradeDate.Result getTargetObject() {
+
+        final GetTradeDate.TradeDate tradeDate = GetTradeDate.TradeDate.builder()
+                .date(DATE)
+                .year(YEAR)
+                .month(MONTH)
+                .day(DAY)
+                .build();
+
+        return new GetTradeDate.Result(tradeDate);
     }
 
     @NotNull
@@ -43,31 +54,15 @@ public class GetTradeDateResultPbSerdeTest extends AbstractResultPbSerdeTest<Get
                 .build();
     }
 
-    @NotNull
-    @Override
-    public GetTradeDate.Result getTargetObject() {
-
-        final GetTradeDate.TradeDate tradeDate = GetTradeDate.TradeDate.builder()
-                .date(DATE)
-                .year(YEAR)
-                .month(MONTH)
-                .day(DAY)
-                .build();
-
-        return new GetTradeDate.Result(tradeDate);
-    }
-
+    // TODO: really shouldn't be a responsibility of the deserializer
     @Test
     public void shouldFailOnUninitializedPbTradeDate() {
 
-        thrown.expect(SerdeException.class);
-        thrown.expectCause(
-                allOf(
-                        instanceOf(IllegalArgumentException.class),
-                        hasProperty("message", equalTo("Поле 'trade_date' экземпляра protobuf-представления qlua.rpc.GetTradeDate.Result не инициализировано."))
-                )
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage(
+                equalTo("Поле 'trade_date' экземпляра protobuf-представления qlua.rpc.GetTradeDate.Result не инициализировано.")
         );
 
-        getSerdeModuleUnderTest().deserialize(GetTradeDate.Result.class, qlua.rpc.GetTradeDate.Result.newBuilder().build().toByteArray());
+        getDeserializerUnderTest().deserialize( qlua.rpc.GetTradeDate.Result.newBuilder().build().toByteArray() );
     }
 }
