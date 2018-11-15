bade7a804f76739bf9842bc2cc2290396653d9ec
Arsentii Nerushev <asnerushev@gmail.com>
2018-11-16 01:27:42 +0300

    Rewrote test for GetFuturesLimit.Result protobuf deserialization.

--- src/test/java/com/enfernuz/quik/lua/rpc/serde/protobuf/GetFuturesLimitResultPbSerdeTest.java
+++ src/test/java/com/enfernuz/quik/lua/rpc/serde/protobuf/GetFuturesLimitResultPbSerdeTest.java
@@ -2,110 +2,88 @@ package com.enfernuz.quik.lua.rpc.serde.protobuf;
 
 import com.enfernuz.quik.lua.rpc.api.messages.GetFuturesLimit;
 import com.enfernuz.quik.lua.rpc.api.structures.FuturesLimit;
-import com.enfernuz.quik.lua.rpc.serde.SerdeModule;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import com.enfernuz.quik.lua.rpc.serde.Deserializer;
+import org.jetbrains.annotations.NotNull;
 import qlua.structs.QluaStructures;
 
-import java.util.Arrays;
+public class GetFuturesLimitResultPbSerdeTest extends AbstractPbDeserializationTest<qlua.rpc.GetFuturesLimit.Result, GetFuturesLimit.Result> {
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+    private static final String FIRM_ID = "1";
+    private static final String TRD_ACC_ID = "2";
+    private static final int LIMIT_TYPE = 3;
+    private static final String LIQUIDITY_COEF = "4";
+    private static final String CBP_PREV_LIMIT = "5";
+    private static final String CBP_LIMIT = "6";
+    private static final String CBPL_USED = "7";
+    private static final String CBPL_PLANNED = "8";
+    private static final String VAR_MARGIN = "9";
+    private static final String ACCRUED_INT = "10";
+    private static final String CBPL_USED_FOR_ORDERS = "11";
+    private static final String CBPL_USED_FOR_POSITIONS = "12";
+    private static final String OPTIONS_PREMIUM = "13";
+    private static final String TS_COMISSION = "14";
+    private static final String KGO = "15";
+    private static final String CURR_CODE = "16";
+    private static final String REAL_VAR_MARGIN = "17";
 
-public class GetFuturesLimitResultPbSerdeTest {
+    @Override
+    public @NotNull Deserializer<GetFuturesLimit.Result> getDeserializerUnderTest() {
+        return GetFuturesLimitResultPbDeserializer.INSTANCE;
+    }
 
-    private static SerdeModule sut;
-
-    private static GetFuturesLimit.Result expectedObject;
-    private static byte[] expectedPbInput;
-
-    private static GetFuturesLimit.Result expectedObjectWithOnlyRequiredFields;
-    private static byte[] expectedPbInputWithOnlyRequiredFields;
-
-    @BeforeClass
-    public static void globalSetup() {
-
-        sut = ProtobufSerdeModule.INSTANCE;
+    @NotNull
+    @Override
+    public GetFuturesLimit.Result getTargetObject() {
 
         final FuturesLimit futuresLimit = FuturesLimit.builder()
-                .firmId("1")
-                .trdAccId("2")
-                .limitType(3)
-                .liquidityCoef("4")
-                .cbpPrevLimit("5")
-                .cbpLimit("6")
-                .cbplUsed("7")
-                .cbplPlanned("8")
-                .varMargin("9")
-                .accruedInt("10")
-                .cbplUsedForOrders("11")
-                .cbplUsedForPositions("12")
-                .optionsPremium("13")
-                .tsComission("14")
-                .kgo("15")
-                .currCode("16")
-                .realVarMargin("17")
+                .firmId(FIRM_ID)
+                .trdAccId(TRD_ACC_ID)
+                .limitType(LIMIT_TYPE)
+                .liquidityCoef(LIQUIDITY_COEF)
+                .cbpPrevLimit(CBP_PREV_LIMIT)
+                .cbpLimit(CBP_LIMIT)
+                .cbplUsed(CBPL_USED)
+                .cbplPlanned(CBPL_PLANNED)
+                .varMargin(VAR_MARGIN)
+                .accruedInt(ACCRUED_INT)
+                .cbplUsedForOrders(CBPL_USED_FOR_ORDERS)
+                .cbplUsedForPositions(CBPL_USED_FOR_POSITIONS)
+                .optionsPremium(OPTIONS_PREMIUM)
+                .tsComission(TS_COMISSION)
+                .kgo(KGO)
+                .currCode(CURR_CODE)
+                .realVarMargin(REAL_VAR_MARGIN)
                 .build();
+
+        return GetFuturesLimit.Result.getInstance(futuresLimit);
+    }
+
+    @NotNull
+    @Override
+    public qlua.rpc.GetFuturesLimit.Result getTargetObjectAsPbMessage() {
+
         final QluaStructures.FuturesLimit pbFuturesLimit = QluaStructures.FuturesLimit.newBuilder()
-                .setFirmid("1")
-                .setTrdaccid("2")
-                .setLimitType(3)
-                .setLiquidityCoef("4")
-                .setCbpPrevLimit("5")
-                .setCbplimit("6")
-                .setCbplused("7")
-                .setCbplplanned("8")
-                .setVarmargin("9")
-                .setAccruedint("10")
-                .setCbplusedForOrders("11")
-                .setCbplusedForPositions("12")
-                .setOptionsPremium("13")
-                .setTsComission("14")
-                .setKgo("15")
-                .setCurrcode("16")
-                .setRealVarmargin("17")
+                .setFirmid(FIRM_ID)
+                .setTrdaccid(TRD_ACC_ID)
+                .setLimitType(LIMIT_TYPE)
+                .setLiquidityCoef(LIQUIDITY_COEF)
+                .setCbpPrevLimit(CBP_PREV_LIMIT)
+                .setCbplimit(CBP_LIMIT)
+                .setCbplused(CBPL_USED)
+                .setCbplplanned(CBPL_PLANNED)
+                .setVarmargin(VAR_MARGIN)
+                .setAccruedint(ACCRUED_INT)
+                .setCbplusedForOrders(CBPL_USED_FOR_ORDERS)
+                .setCbplusedForPositions(CBPL_USED_FOR_POSITIONS)
+                .setOptionsPremium(OPTIONS_PREMIUM)
+                .setTsComission(TS_COMISSION)
+                .setKgo(KGO)
+                .setCurrcode(CURR_CODE)
+                .setRealVarmargin(REAL_VAR_MARGIN)
                 .build();
 
-        expectedObject = GetFuturesLimit.Result.getInstance(futuresLimit);
-        expectedPbInput = qlua.rpc.GetFuturesLimit.Result.newBuilder()
+        return qlua.rpc.GetFuturesLimit.Result.newBuilder()
                 .setFuturesLimit(pbFuturesLimit)
-                .build()
-                .toByteArray();
-
-        expectedObjectWithOnlyRequiredFields = GetFuturesLimit.Result.getInstance(null);
-        expectedPbInputWithOnlyRequiredFields = qlua.rpc.GetFuturesLimit.Result.newBuilder().build().toByteArray();
-    }
-
-    @Test
-    public void testSerialize() {
-
-        assertTrue(
-                Arrays.equals(expectedPbInput, sut.serialize(expectedObject))
-        );
-    }
-
-    @Test
-    public void testDeserialize() {
-        assertEquals(expectedObject, sut.deserialize(GetFuturesLimit.Result.class, expectedPbInput));
-    }
-
-    @Test
-    public void testSerialize_WithOnlyRequiredFields() {
-
-        assertTrue(
-                Arrays.equals(
-                        expectedPbInputWithOnlyRequiredFields,
-                        sut.serialize(expectedObjectWithOnlyRequiredFields)
-                )
-        );
-    }
-
-    @Test
-    public void testDeserialize_WithOnlyRequiredFields() {
-
-        assertEquals(
-                expectedObjectWithOnlyRequiredFields,
-                sut.deserialize(GetFuturesLimit.Result.class, expectedPbInputWithOnlyRequiredFields)
-        );
+                .build();
     }
 }
