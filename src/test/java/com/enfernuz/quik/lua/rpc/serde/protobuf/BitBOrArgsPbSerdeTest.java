15a27615739dd7bf79075aa42beb12d9896f933e
Arsentii Nerushev <asnerushev@gmail.com>
2019-01-27 23:01:03 +0300

    Added test for bit.BOr.Args protobuf serialization.

---
+++ src/test/java/com/enfernuz/quik/lua/rpc/serde/protobuf/BitBOrArgsPbSerdeTest.java
@@ -0,0 +1,78 @@
+package com.enfernuz.quik.lua.rpc.serde.protobuf;
+
+import com.enfernuz.quik.lua.rpc.api.messages.bit.BOr;
+import com.enfernuz.quik.lua.rpc.serde.Serializer;
+import org.jetbrains.annotations.NotNull;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
+
+import java.util.Arrays;
+
+@RunWith(Enclosed.class)
+public class BitBOrArgsPbSerdeTest {
+
+    public static class FullArgsBitBOrArgsPbSerdeTest extends AbstractPbSerializationTest<BOr.Args, qlua.rpc.bit.Bor.Request> {
+
+
+        private static final int X1 = 1;
+        private static final int X2 = 2;
+        private static final int[] XI = new int[] {3, 4, 5};
+
+        @Override
+        public @NotNull Serializer<BOr.Args> getSerializerUnderTest() {
+            return BOrArgsPbSerializer.INSTANCE;
+        }
+
+        @NotNull
+        @Override
+        public qlua.rpc.bit.Bor.Request getTargetObjectAsPbMessage() {
+
+            return qlua.rpc.bit.Bor.Request.newBuilder()
+                    .setX1(X1)
+                    .setX2(X2)
+                    .addAllXi( Arrays.asList(3, 4, 5) )
+                    .build();
+        }
+
+        @NotNull
+        @Override
+        public BOr.Args getTargetObject() {
+            return BOr.Args.builder()
+                    .x1(X1)
+                    .x2(X2)
+                    .xi(XI)
+                    .build();
+        }
+    }
+
+    public static class WithoutXiBitBOrArgsPbSerdeTest extends AbstractPbSerializationTest<BOr.Args, qlua.rpc.bit.Bor.Request> {
+
+
+        private static final int X1 = 1;
+        private static final int X2 = 2;
+
+        @Override
+        public @NotNull Serializer<BOr.Args> getSerializerUnderTest() {
+            return BOrArgsPbSerializer.INSTANCE;
+        }
+
+        @NotNull
+        @Override
+        public qlua.rpc.bit.Bor.Request getTargetObjectAsPbMessage() {
+
+            return qlua.rpc.bit.Bor.Request.newBuilder()
+                    .setX1(X1)
+                    .setX2(X2)
+                    .build();
+        }
+
+        @NotNull
+        @Override
+        public BOr.Args getTargetObject() {
+            return BOr.Args.builder()
+                    .x1(X1)
+                    .x2(X2)
+                    .build();
+        }
+    }
+}
